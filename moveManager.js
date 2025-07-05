b61ef3838b9d0686729e13ff7b3796904673e549
zinq <benfarmilo@gmail.com>
2025-07-05 01:13:03 -0400

    main cleanup + move manager

--- moveManager.js
+++ moveManager.js
@@ -1,3 +1,41 @@
+import { adjustViewport, updateCamera } from "./cameraManager.js";
+import { camera, showCamera, canvas } from "./internal.js";
+
+// CONSIDER: acceleration
+const SPEED = 5;
+
+export function setupMovement() {
+    document.onkeydown = (e) => {
+        if (e.key == "d") {
+            move(1, 0);
+        } else if (e.key == "s") {
+            move(0, 1);
+        } else if (e.key == "a") {
+            move(-1, 0);
+        } else if (e.key == "w") {
+            move(0, -1);
+        }
+    }
+}
+
+function move(dx, dy) {
+    
+    const { x, y } = camera;
+    let newX = x + SPEED * dx;
+    let newY = y + SPEED * dy;
+
+    // semi lazy boundary detection -- camera straight stops fr fr
+    newX = Math.min(newX + camera.width, canvas.width) - camera.width;
+    newX = Math.max(newX, 0);
+    newY = Math.min(newY + camera.height, canvas.height) - camera.height;
+    newY = Math.max(newY, 0);
+
+    updateCamera(newX, newY);
+
+    showCamera();
+    adjustViewport();
+}
+
 export function hello() {
     console.log("hello world");
 }
--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 import { setupCanvas, showCamera } from "./canvasManager.js";
 import { adjustViewport, getCam, updateCamera, setupViewport } from './cameraManager.js';
+import { setupMovement } from "./moveManager.js";
+
 
 setupCanvas();
 showCamera();
 
 setupViewport();
-
-document.onkeydown = (e) => {
-    console.log(e.key);
-
-    const { x, y } = getCam();
-    if (e.key == "d") {
-        updateCamera(x + 5, y);
-    } else if (e.key == "s") {
-        updateCamera(x, y + 5);
-    } else if (e.key == "a") {
-        updateCamera(x - 5, y);
-    } else if (e.key == "w") {
-        updateCamera(x, y - 5);
-    }
-
-    showCamera(getCam());
-
-    adjustViewport();
-}
+setupMovement();
